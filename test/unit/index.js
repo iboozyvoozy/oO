8261710aefaaf8df157bfef7bc37121fd30d3f84
iboozyvoozy <iboozyvoozy@me.com>
2016-09-09 16:43:06 +0300

    fix(karma): fix path after removing ROOT from webpack config

--- test/unit/index.js
+++ test/unit/index.js
@@ -2,5 +2,5 @@ import 'angular-mocks';
 
 const importAll = ctx => ctx.keys().map(ctx);
 
-importAll(require.context('src/', true, /\.spec\.js$/));
-importAll(require.context('test/unit/', true, /\.spec\.js$/));
+importAll(require.context('../../src/', true, /\.spec\.js$/));
+importAll(require.context('.', true, /\.spec\.js$/));
